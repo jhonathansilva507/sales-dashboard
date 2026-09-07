505d9a84be837669c5f225f52de4fe2e975fd1d3
Jhonathan Gabriel <jhgabriel507@gmail.com>
2026-09-07 16:16:33 -0300

    style: improve navbar styling and responsiveness
    - Add mobile responsive layout with dropdown menu
    - Implement hover effects for desktop view
    - Add user avatar icon

--- src/components/common/Sidebar.tsx
+++ src/components/common/Sidebar.tsx
@@ -1,19 +1,79 @@
-// import {NavLink} from 'react-router'
+import { Menu, X } from "lucide-react";
+import { Avatar, AvatarImage, AvatarFallback } from "../ui/avatar";
+import { useState } from "react";
 
 const Sidebar = () => {
-  return (
-    <aside className='max-w-2xs min-h-screen bg-blue-900 flex flex-col text-white p-7'>
-        <a>
-            Dashboard
-        </a>
-        <a>
-            Cadastrar produto
-        </a>
-        <a>
-            Gerenciar compras
-        </a>
-    </aside>
-  )
-}
+  const [openMenu, setOpenMenu] = useState(false);
 
-export default Sidebar
+  return (
+    <aside className="lg:w-51 bg-blue-900 text-white p-7 sticky top-0 left-0 z-50 shadow-md">
+      <div className="flex lg:flex-col items-center justify-between lg:items-start lg:gap-3">
+        <div className="">
+          <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+        </div>
+        <button
+          onClick={() => setOpenMenu(!openMenu)}
+          className="relative cursor-pointer lg:hidden size-8"
+        >
+          <X
+            className={`absolute inset-0 m-auto transition-all duration-150 ${openMenu ? "rotate-0 scale-100 opacity-100" : "-rotate-90 scale-0 opacity-0"}`}
+          />
+          <Menu
+            className={`absolute inset-0 m-auto transition-all duration-150 ${openMenu ? "rotate-90 scale-0 opacity-0" : "rotate-0 scale-100 opacity-100"}`}
+          />
+        </button>
+      </div>
+      <div
+        className={`grid transition-[grid-template-rows, opacity] duration-300 ${openMenu ? "grid-rows-[1fr] opacity-100" : "grid-rows-[0fr] opacity-0"} lg:opacity-100 lg:grid-rows-[1fr]`}
+      >
+        <div className="min-h-0 overflow-hidden">
+          <nav
+            className={`flex flex-col lg:items-start items-center gap-2 mt-4 pt-2 border-t border-blue-200/30 lg:border-none`}
+          >
+            <a
+              className="
+              relative
+              transition-all
+              duration-300
+              cursor-pointer
+              
+              before:absolute before:left-0 before:w-px before:h-0 hover:before:h-4 before:top-1/2 before:-translate-y-1/2 hover:pl-2 before:bg-white before:transition-all before:duration-400
+              "
+            >
+              Dashboard
+            </a>
+            <a
+              className="
+              relative
+              transition-all
+              duration-300
+              cursor-pointer
+              
+              before:absolute before:left-0 before:w-px before:h-0 hover:before:h-4 before:top-1/2 before:-translate-y-1/2 hover:pl-2 before:bg-white before:transition-all before:duration-400
+              "
+            >
+              Cadastrar produto
+            </a>
+            <a
+              className="
+              relative
+              transition-all
+              duration-300
+              cursor-pointer
+              
+              before:absolute before:left-0 before:w-px before:h-0 hover:before:h-4 before:top-1/2 before:-translate-y-1/2 hover:pl-2 before:bg-white before:transition-all before:duration-400
+              "
+            >
+              Gerenciar compras
+            </a>
+          </nav>
+        </div>
+      </div>
+    </aside>
+  );
+};
+
+export default Sidebar;
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -3,7 +3,7 @@ import Sidebar from "../common/Sidebar";
 
 const Layout = () => {
   return (
-    <div className="flex min-h-screen h-full">
+    <div className="lg:flex min-h-screen">
       <Sidebar />
       <main className="p-8 flex flex-1 flex-col bg-slate-100 min-h-screen">
         <Outlet />
